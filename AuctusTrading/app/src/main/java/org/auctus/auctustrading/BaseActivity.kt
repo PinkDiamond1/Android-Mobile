035cf028e7996c745861f127b24bb14c9d368da9
danielduartef <danielduartefigueiredo@gmail.com>
2019-01-04 18:55:58 -0200

    base activity with progress bar

---
+++ AuctusTrading/app/src/main/java/org/auctus/auctustrading/BaseActivity.kt
@@ -0,0 +1,38 @@
+package org.auctus.auctustrading
+
+import android.opengl.Visibility
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.annotation.LayoutRes
+import android.view.View
+import android.view.ViewStub
+import android.widget.ProgressBar
+
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    @LayoutRes
+    abstract fun getContentLayoutId(): Int
+
+    abstract fun onCreateContent(content: View)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+
+        val vs = findViewById<ViewStub>(R.id.stub)
+
+        vs.layoutResource = getContentLayoutId()
+        val mainContent = vs.inflate()
+
+        onCreateContent(mainContent)
+    }
+
+    protected fun showProgress() {
+        findViewById<ProgressBar>(R.id.simpleProgressBar).visibility = View.VISIBLE
+    }
+
+    protected fun hideProgress() {
+        findViewById<ProgressBar>(R.id.simpleProgressBar).visibility = View.INVISIBLE
+    }
+}
