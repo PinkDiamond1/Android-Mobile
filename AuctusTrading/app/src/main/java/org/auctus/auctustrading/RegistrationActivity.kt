f5dc38de19a956527361c428bba2dd9a7cbd8f37
danielduartef <danielduartefigueiredo@gmail.com>
2019-01-07 18:39:34 -0200

    layout of registration screen

--- AuctusTrading/app/src/main/java/org/auctus/auctustrading/RegistrationActivity.kt
+++ AuctusTrading/app/src/main/java/org/auctus/auctustrading/RegistrationActivity.kt
@@ -2,7 +2,10 @@ package org.auctus.auctustrading
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : BaseActivity() {
 
@@ -11,6 +14,18 @@ class RegistrationActivity : BaseActivity() {
     }
 
     override fun onCreateContent(content: View) {
+        editDescription.addTextChangedListener(object : TextWatcher {
 
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                description_counter.setText("${s.length} / 160")
+            }
+        })
     }
 }
