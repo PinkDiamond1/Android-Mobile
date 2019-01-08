50780cc56ea18e193a1880153a33514bba8ee6d9
danielduartef <danielduartefigueiredo@gmail.com>
2019-01-08 18:10:29 -0200

    server api calls on BaseActivity

--- AuctusTrading/app/src/main/java/org/auctus/auctustrading/BaseActivity.kt
+++ AuctusTrading/app/src/main/java/org/auctus/auctustrading/BaseActivity.kt
@@ -1,13 +1,18 @@
 package org.auctus.auctustrading
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.LayoutRes
+import android.support.v7.app.AlertDialog
 import android.view.View
 import android.view.ViewStub
 import android.widget.ProgressBar
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
 
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -41,5 +46,32 @@ abstract class BaseActivity : AppCompatActivity() {
     protected fun navigate(activityClass: Class<*>) {
         val intent = Intent(this, activityClass)
         startActivity(intent)
+    }
+
+    protected fun sendPostRequest(url: String, successListener: Response.Listener<String>) {
+        sendRequest(Request.Method.POST, url, successListener)
+    }
+
+    protected fun sendGetRequest(url: String, successListener: Response.Listener<String>) {
+        sendRequest(Request.Method.GET, url, successListener)
+    }
+
+    protected fun sendRequest(requestMethod: Int, url: String, successListener: Response.Listener<String>) {
+        val stringRequest = StringRequest(requestMethod, "${BuildConfig.SERVER_URL}${url}",
+                successListener, getErrorDialogListener())
+
+        RequestQueueSingleton.getInstance(this.applicationContext).addToRequestQueue(stringRequest)
+    }
+
+    private fun getErrorDialogListener() : Response.ErrorListener {
+        return Response.ErrorListener {
+            val alertDialog = AlertDialog.Builder(this).create()
+            alertDialog.setTitle("Alert")
+            alertDialog.setMessage("The request could not be processed")
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    DialogInterface.OnClickListener { dialog, which -> dialog.dismiss() })
+            alertDialog.show()
+            hideProgress()
+        }
     }
 }
--- AuctusTrading/app/src/main/java/org/auctus/auctustrading/HomeActivity.kt
+++ AuctusTrading/app/src/main/java/org/auctus/auctustrading/HomeActivity.kt
@@ -1,10 +1,6 @@
 package org.auctus.auctustrading
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : BaseActivity() {
 
