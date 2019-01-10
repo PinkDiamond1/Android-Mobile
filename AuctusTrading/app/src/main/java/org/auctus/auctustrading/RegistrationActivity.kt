fd5feb15a7bfe3577ef82ed6742397211dbce6c6
danielduartef <danielduartefigueiredo@gmail.com>
2019-01-10 11:49:31 -0200

    api calls

--- AuctusTrading/app/src/main/java/org/auctus/auctustrading/RegistrationActivity.kt
+++ AuctusTrading/app/src/main/java/org/auctus/auctustrading/RegistrationActivity.kt
@@ -5,7 +5,12 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
 import kotlinx.android.synthetic.main.activity_registration.*
+import org.json.JSONObject
 
 class RegistrationActivity : BaseActivity() {
 
@@ -27,5 +32,47 @@ class RegistrationActivity : BaseActivity() {
                 description_counter.setText("${s.length} / 160")
             }
         })
+    }
+
+    fun onClickSubmit(v: View) {
+        showProgress()
+        val params = JSONObject()
+        params.put("name", editName.text)
+        params.put("email", editEmail.text)
+        params.put("password", editPassword.text)
+        params.put("description", editDescription.text)
+        params.put("referralCode", editInvitation.text)
+        params.put("fromMobile", true)
+
+        sendPostRequest("v1/advisors",
+                params,
+                Response.Listener<JSONObject> { response ->
+                    hideProgress()
+                    navigate(HomeActivity::class.java)
+                })
+    }
+
+    fun submit(v: View) {
+        val url = "${BuildConfig.SERVER_URL}v1/advisors"
+        val jsonObjRequest = object : StringRequest(Request.Method.POST,
+                url,
+                Response.Listener<String> { response ->
+                    hideProgress()
+                    navigate(HomeActivity::class.java)
+                },
+                getErrorDialogListener()) {
+
+            override fun getBodyContentType(): String {
+                return "application/x-www-form-urlencoded; charset=UTF-8"
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getBody(): ByteArray {
+                return "name=aa&email=aa@aaa.com&password=bb&description=xulambis&fromMobile=true".toByteArray()
+            }
+
+        }
+
+        RequestQueueSingleton.getInstance(this.applicationContext).addToRequestQueue(jsonObjRequest)
     }
 }
