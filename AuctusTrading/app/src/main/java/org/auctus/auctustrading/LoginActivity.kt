1c84df47848b07ebc58764902ad7e47211b14b10
danielduartef <danielduartefigueiredo@gmail.com>
2019-01-14 15:11:51 -0200

    login api call

--- AuctusTrading/app/src/main/java/org/auctus/auctustrading/LoginActivity.kt
+++ AuctusTrading/app/src/main/java/org/auctus/auctustrading/LoginActivity.kt
@@ -1,6 +1,9 @@
 package org.auctus.auctustrading
 
 import android.view.View
+import com.android.volley.Response
+import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
     override fun getContentLayoutId(): Int {
@@ -13,5 +16,30 @@ class LoginActivity : BaseActivity() {
 
     fun navigateToForgotPassword(v: View){
         navigate(ForgotPasswordActivity::class.java)
+    }
+
+    fun onClickEnter(v: View) {
+        if (validate()) {
+            inputLayoutEmail.isErrorEnabled = false
+            showProgress()
+            val params = JSONObject()
+            params.put("email", emailInput.text)
+            params.put("password", passwordInput.text)
+            params.put("fromMobile", true)
+            sendPostRequest("v1/accounts/login",
+                    params,
+                    Response.Listener<JSONObject> { response ->
+                        hideProgress()
+                        navigate(HomeActivity::class.java)
+                    })
+        }
+        else {
+            inputLayoutEmail.isErrorEnabled = true
+            inputLayoutEmail.error = "Please inform a valid email address."
+        }
+    }
+
+    private fun validate() : Boolean {
+        return ValidationUtil.isValidEmail(emailInput.text)
     }
 }
