d630ef66b8e188ec11e43d388412c729a50f8b54
Daniel Silveira <dansilveira@gmail.com>
2019-01-14 18:26:35 -0200

    Select picture

--- AuctusTrading/app/src/main/java/org/auctus/auctustrading/RegistrationActivity.kt
+++ AuctusTrading/app/src/main/java/org/auctus/auctustrading/RegistrationActivity.kt
@@ -2,8 +2,6 @@ package org.auctus.auctustrading
 
 import android.Manifest
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
@@ -12,19 +10,26 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import kotlinx.android.synthetic.main.activity_registration.*
-import android.Manifest.permission
 import android.content.pm.PackageManager
 import android.app.Activity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.DialogFragment
 import android.support.v4.content.ContextCompat
 import org.json.JSONObject
+import java.io.ByteArrayOutputStream
 
 
-private val MY_PERMISSIONS_REQUEST_USE_CAMERA = 0x00AF
+private const val PERMISSIONS_REQUEST_USE_CAMERA = 0x00AF
+private const val PERMISSIONS_REQUEST_READ_STORAGE = 0X00B0
+
 class RegistrationActivity : BaseActivity() {
     val PICK_IMAGE = 0
     val TAKE_PICTURE = 1
 
+    var cameraGalleryPicker: DialogFragment? = null
+    var encodedSelectedImage  = ""
     override fun getContentLayoutId(): Int {
         return R.layout.activity_registration
     }
@@ -44,51 +49,99 @@ class RegistrationActivity : BaseActivity() {
             }
         })
     }
-    fun onGalleryClicked(view: View) {
-        val intent = Intent();
-        intent.type = "image/*";
-        intent.action = Intent.ACTION_GET_CONTENT;
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    fun onAddPictureClick(view: View){
+        val fm = supportFragmentManager
+        cameraGalleryPicker = CameraGaleryPicker.newInstance()
+        cameraGalleryPicker?.show(fm, "fragment_camera_gallery")
     }
 
-    fun onPictureClicked(view: View) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA ) != PackageManager.PERMISSION_GRANTED) {
+    fun onGalleryClicked(view: View) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_READ_STORAGE);
+        } else {
+            launchGalleryIntent()
+        }
+    }
+
+    private fun launchGalleryIntent(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+    }
+
+    fun onTakePictureClicked(view: View) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.CAMERA), MY_PERMISSIONS_REQUEST_USE_CAMERA);
+                    arrayOf(Manifest.permission.CAMERA), PERMISSIONS_REQUEST_USE_CAMERA);
         } else {
-            val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(cameraIntent, TAKE_PICTURE)
+            launchCameraIntent()
         }
-
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
+    private fun launchCameraIntent(){
+        Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+                .also { takePictureIntent ->
+                    takePictureIntent.resolveActivity(packageManager)?.also {
+                        startActivityForResult(takePictureIntent, TAKE_PICTURE)
+                    }
+                }
+    }
+
+    fun onRemovePictureClicked(view: View) {
+        registration_avatar.setImageDrawable(ContextCompat.getDrawable(applicationContext, R.drawable.avatar ))
+        encodedSelectedImage = ""
+        cameraGalleryPicker?.dismissAllowingStateLoss()
+    }
+
+   override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>,
                                             grantResults: IntArray) {
         when (requestCode) {
-            MY_PERMISSIONS_REQUEST_USE_CAMERA -> {
+            PERMISSIONS_REQUEST_USE_CAMERA -> {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
-                    startActivityForResult(cameraIntent, TAKE_PICTURE)
-                } else {
+                    launchCameraIntent()
+                }
+                return
+            }
+            PERMISSIONS_REQUEST_READ_STORAGE ->{
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    launchGalleryIntent()
                 }
                 return
             }
         }
     }
 
-    fun onAddPictureClick(view: View){
-        val fm = supportFragmentManager
-        val cameraGalleryPicker = CameraGaleryPicker.newInstance()
-        cameraGalleryPicker.show(fm, "fragment_camera_gallery")
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+         if (requestCode == TAKE_PICTURE && resultCode == Activity.RESULT_OK) {
+             val imageBitmap = data?.extras?.get("data") as Bitmap
+             registration_avatar.setImageBitmap(imageBitmap)
+             encodedSelectedImage = encodeImage(imageBitmap)
+             cameraGalleryPicker?.dismissAllowingStateLoss()
+         }
+
+         else if ( requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+             val imageUri = data?.data
+             val imageStream = contentResolver.openInputStream(imageUri)
+             val imageBitmap = BitmapFactory.decodeStream(imageStream)
+             registration_avatar.setImageBitmap(imageBitmap)
+             encodedSelectedImage = encodeImage(imageBitmap)
+             cameraGalleryPicker?.dismissAllowingStateLoss()
+         }
+     }
+
+    private fun encodeImage(bm: Bitmap): String {
+        val baos = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val bArray = baos.toByteArray()
+
+        return android.util.Base64.encodeToString(bArray, android.util.Base64.DEFAULT)
     }
-
-     fun onActivityResult(requestCode: Int) {
-        if (requestCode == PICK_IMAGE) {
-
-        }
 
     fun onClickSubmit(v: View) {
         showProgress()
